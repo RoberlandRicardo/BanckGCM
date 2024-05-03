2beeda3dc576aaa057480d9021033f55bf774d2a
RoberlandRicardo <roberland.ricardo@gmail.com>
2024-05-03 17:56:53 -0300

    Adicionando identificado e saldo a classe conta  #2

--- src/model/Account.java
+++ src/model/Account.java
@@ -1,5 +1,27 @@
 package model;
 
 public class Account {
-
+	private int identf;
+	private float balance;
+	
+	public int getIdentf() {
+		return identf;
+	}
+	
+	public void setIdentf(int identf) {
+		this.identf = identf;
+	}
+	
+	public float getBalance() {
+		return balance;
+	}
+	
+	public void increasseBalance(float balance) {
+		this.balance += balance;
+	}
+	
+	public void decreasseBalance(float balance) {
+		this.balance -= balance;
+	}
+	
 }
