17f9bf4d44b945a77b79cab6929afc40b5491871
RoberlandRicardo <roberland.ricardo@gmail.com>
2024-05-03 01:00:36 -0300

    Exibindo menu #1

--- src/program/Interface.java
+++ src/program/Interface.java
@@ -2,11 +2,25 @@ package program;
 
 public class Interface {
 	
-	static public void init() {
-		
+	public void init() {
+		showMenu();
 	}
 	
-	private void showMenu() {
+	static private void showMenu() {
+		System.out.print(
+			  "****************************\n"
+			+ "Bem vindo ao Banco do Brasil\n"
+			+ "****************************\n"
+			+ "\n"
+			+ "- Digite o número ao lado da opção para escolhe-la:\n"
+			+ "\n"
+			+ "1 - Cadastrar nova conta\n"
+			+ "2 - Consultar saldo\n"
+			+ "3 - Fazer depósito\n"
+			+ "4 - Realizar saque\n"
+			+ "5 - Realizar transferência\n"
+			
+		);
 		
 	}
 }
--- src/program/Program.java
+++ src/program/Program.java
@@ -3,8 +3,8 @@ package program;
 public class Program {
 
 	public static void main(String[] args) {
-		
-
+		Interface intfc = new Interface();
+		intfc.init();
 	}
 
 }
